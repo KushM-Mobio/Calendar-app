2cccc98e3759128d9e484f107346c577f6aba4e7
KushM-Mobio <kush.mistry@mobiosolutions.com>
2023-07-18 00:35:54 -0400

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Calender App
+
+calender app
+
+#### License
+
+MIT
---
+++ calender_app/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Calender App",
+			"type": "module",
+			"label": _("Calender App")
+		}
+	]
